9090ec0dd4a472f0a8375cb386bf2311a7000fa3
MintCat <presidentmc9898@gmail.com>
2024-02-01 13:56:35 +0900

    [Add] Connection to Detailed Pages

--- src/main/java/org/mintcat/springbootexercise/question/QuestionController.java
+++ src/main/java/org/mintcat/springbootexercise/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -21,5 +22,12 @@ public class QuestionController {
     List<Question> questionList = this.questionService.getList();
     model.addAttribute("questionList", questionList);
     return "question_list";
+  }
+
+  @GetMapping(value = "/question/detail/{id}")
+  public String detail(Model model, @PathVariable("id") Integer id) {
+    Question question = this.questionService.getQuestion(id);
+    model.addAttribute("question", question);
+    return "question_detail";
   }
 }
--- src/main/java/org/mintcat/springbootexercise/question/QuestionService.java
+++ src/main/java/org/mintcat/springbootexercise/question/QuestionService.java
@@ -1,9 +1,11 @@
 package org.mintcat.springbootexercise.question;
 
 import lombok.RequiredArgsConstructor;
+import org.mintcat.springbootexercise.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +14,14 @@ public class QuestionService {
 
   public List<Question> getList() {
     return this.questionRepository.findAll();
+  }
+
+  public Question getQuestion(Integer id) {
+    Optional<Question> question = this.questionRepository.findById(id);
+    if (question.isPresent()){
+      return question.get();
+    } else{
+      throw new DataNotFoundException("question is not found");
+    }
   }
 }
